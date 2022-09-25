721fa3b01bf1943b7a389b8af491918cceedf562
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2022-09-25 18:28:44 -0300

    Adicionado no index.php e copiado .vercel.json

--- api/index.php
+++ api/index.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
   </head>
   <body>
-    <h1>Estamos Trabalhando</h1>
+    <h1>Estamos Trabalhando</h1><br>
+    <div data-ls-module="progressBar" role="progressbar" aria-valuenow="100" class="ls-left-percentage"></div>
+
   </body>
 </html>
