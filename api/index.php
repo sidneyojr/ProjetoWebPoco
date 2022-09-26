e75c44c2513109203b0e2220718b92c0e3975a22
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2022-09-26 00:30:00 -0300

    Atualizado index.php

--- api/index.php
+++ api/index.php
@@ -2,7 +2,27 @@
 <html lang="pt-br">
   <head>
     <title>Index.php</title>
-    <link rel="stylesheet" type="text/css" href="../css/stylesheet.css" media="screen" />
+    <style type="text/css">
+      html, body{
+    padding: 3px;
+    margin: 0;
+    min-height: 100px;
+  }
+  
+  .progress-bar{
+    height: 50px;
+    padding: 5px;
+    background-color: #ccc;
+    display: flex;
+  }
+  
+  .progress-bar::before{
+    content: "";
+    width: 50%;
+    background-color: red;
+  }
+
+    </style>
     <meta charset="utf-8">
   </head>
   <body>
